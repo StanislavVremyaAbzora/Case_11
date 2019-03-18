9ae026423a7d29b5954d64f8709ed0c2779f0b41
666stephunter <44421376+666stephunter@users.noreply.github.com>
2019-03-18 15:12:48 +0700

    Update Case_11.py

--- Case_11.py
+++ Case_11.py
@@ -1,16 +1,16 @@
 '''Функция для построения убегающего квадрата'''
-import turtle
+from turtle import *
 
 def k(d, n):
     if n == 0:
         return
-    turtle.right(20)
-    turtle.up()
-    turtle.forward(d / 4)
-    turtle.down()
+    right(20)
+    up()
+    forward(d / 4)
+    down()
     for x in range(4):
-        turtle.forward(d)
-        turtle.right(90)
+        forward(d)
+        right(90)
     return k(0.8 * d, n -1)
 
 def main():
